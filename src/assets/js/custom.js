2ddad4a752df1f1fbae9256f3b06ba4db9b42e9f
Your Name <you@example.com>
2022-06-07 18:03:21 +0530

    Flight searching - passenger and class dropdown integration

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -28,11 +28,11 @@ $(document).ready(function($) {
         e.stopPropagation();
     });
 
-    $('.single-datepicker').daterangepicker({
-        singleDatePicker: true,
-        opens: 'center'
-    });
-    
+    // $('.single-datepicker').daterangepicker({
+    //     singleDatePicker: true,
+    //     opens: 'center'
+    // });
+
     $('.daterange-right').daterangepicker({
         opens: 'center'
     });
@@ -45,22 +45,22 @@ $(document).ready(function($) {
             c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
         })
     }
-    String.prototype.getDecimals || (String.prototype.getDecimals = function() {
-        var a = this,
-            b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-        return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
-    }), jQuery(document).ready(function() {
-        wcqib_refresh_quantity_increments()
-    }), jQuery(document).on("updated_wc_div", function() {
-        wcqib_refresh_quantity_increments()
-    }), jQuery(document).on("click", ".plus, .minus", function() {
-        var a = jQuery(this).closest(".quantity").find(".qty"),
-            b = parseFloat(a.val()),
-            c = parseFloat(a.attr("max")),
-            d = parseFloat(a.attr("min")),
-            e = a.attr("step");
-        b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
-    });
+    // String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+    //     var a = this,
+    //         b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    //     return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+    // }), jQuery(document).ready(function() {
+    //     wcqib_refresh_quantity_increments()
+    // }), jQuery(document).on("updated_wc_div", function() {
+    //     wcqib_refresh_quantity_increments()
+    // }), jQuery(document).on("click", ".plus, .minus", function() {
+    //     var a = jQuery(this).closest(".quantity").find(".qty"),
+    //         b = parseFloat(a.val()),
+    //         c = parseFloat(a.attr("max")),
+    //         d = parseFloat(a.attr("min")),
+    //         e = a.attr("step");
+    //     b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
+    // });
 
 
 
@@ -76,9 +76,9 @@ $(document).ready(function($) {
     //     $(".flight-to-data").toggleClass("flight-from-hide");
     // });
 
-    $(".select-root-right h5").click(function(){
-        $(".flight-to-data").toggleClass("flight-from-hide");
-    });
+    // $(".select-root-right h5").click(function(){
+    //     $(".flight-to-data").toggleClass("flight-from-hide");
+    // });
 
     var owlslider = jQuery(".carousel-search-date");
     if (owlslider.length > 0) {
@@ -128,7 +128,7 @@ $(document).ready(function($) {
             });
         });
     }
-    
+
 
     /*-------------------------------------------------------------
     :: window Ready
@@ -160,7 +160,7 @@ $(document).ready(function($) {
             ScrollTop.addClass("active");
         }
         // :: back to top JS End
-    }); 
+    });
 
     $(window).on("load",function(){
         $(".content").mCustomScrollbar();
