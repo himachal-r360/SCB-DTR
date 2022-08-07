93dae090f8b1e8bddb6ab37f9d424fc6396023a9
TamilSelvan <tamil.selvan@reward360.co>
2022-08-07 11:09:45 +0530

    Code merged

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -18,7 +18,8 @@ import { FlightService } from '../common/flight.service';
 import { StyleManagerService } from 'src/app/shared/services/style-manager.service';
 import { SimpleGlobal } from 'ng2-simple-global';
 import {environment} from '../../environments/environment';
-import { MatDatepicker } from '@angular/material/datepicker'
+import { MatDatepicker } from '@angular/material/datepicker';
+import { ElasticsearchService } from 'src/app/shared/services/elasticsearch.service';
 import * as moment from 'moment';
 declare var $: any;
 export const MY_DATE_FORMATS = {
@@ -94,7 +95,13 @@ export class FlightSearchComponent implements OnInit,  OnDestroy {
   navItemActive:any;
   isShowPartner = false;
   sameCity;
-
+private lastKeypress = 0;
+private queryText = '';
+flightFromOptions: any[];
+flightToOptions: any[];
+defaultFlightOptions: any[];
+        searchFlightFromHeader:string="Popular Cities";
+        searchFlightToHeader:string="Popular Cities";
   flightData: any = this._fb.group({
     flightfrom: ['',[Validators.required]],
     flightto: ['',[Validators.required]],
@@ -122,7 +129,7 @@ export class FlightSearchComponent implements OnInit,  OnDestroy {
     public route: ActivatedRoute,
       public router: Router,
       private _fb: FormBuilder,
-      private _flightService: FlightService,private ngZone:NgZone,private sg: SimpleGlobal
+      private _flightService: FlightService,private ngZone:NgZone,private sg: SimpleGlobal,private es: ElasticsearchService
 
     ) {
       this.cdnUrl = environment.cdnUrl+this.sg['assetPath'];
@@ -134,11 +141,21 @@ export class FlightSearchComponent implements OnInit,  OnDestroy {
           });
       }
       setTimeout(() => {
-        this._styleManager.setStyle('bootstrap-select', `assets/css/bootstrap-select.min.css`);
-        this._styleManager.setStyle('daterangepicker', `assets/css/daterangepicker.css`);
-        this._styleManager.setScript('bootstrap-select', `assets/js/bootstrap-select.min.js`);
         this._styleManager.setScript('custom', `assets/js/custom.js`);
      }, 10);
+     
+     	this.defaultFlightOptions=[
+	{"_source":{"city":"New Delhi","airport_code":"DEL","airport_name":"Indira Gandhi Airport","country_code":"IN"}},
+	{"_source":{"city":"Mumbai","airport_code":"BOM","airport_name":"Chatrapati Shivaji Airport","country_code":"IN"}},
+	{"_source":{"city":"Bangalore","airport_code":"BLR","airport_name":"Kempegowda International Airport","country_code":"IN"}},
+	{"_source":{"city":"Goa","airport_code":"GOI","airport_name":"Dabolim Airport","country_code":"IN"}},
+	{"_source":{"city":"Chennai","airport_code":"MAA","airport_name":"Chennai Airport","country_code":"IN"}},
+	{"_source":{"city":"Kolkata","airport_code":"CCU","airport_name":"Netaji Subhas Chandra Bose Airport","country_code":"IN"}},
+	{"_source":{"city":"Hyderabad","airport_code":"HYD","airport_name":"Hyderabad Airport","country_code":"IN"}},
+	];
+     this.flightFromOptions= this.defaultFlightOptions;
+	this.flightToOptions= this.defaultFlightOptions;
+
 
     }
 
@@ -147,7 +164,7 @@ export class FlightSearchComponent implements OnInit,  OnDestroy {
   };
   ngOnInit(): void {
          this.route.url.subscribe(url =>{
-console.log(this.isViewPartner)
+
    this._flightService.showHeader(true);
    this.displayPartners = this.isViewPartner=="false" ? false : true;
     this.isMobile = window.innerWidth < 991 ?  true : false;
@@ -162,6 +179,7 @@ console.log(this.isViewPartner)
 
 
   currentPeriodClicked(datePicker:any){
+   /*
     let date = datePicker.target.value
     if(date && this.navItemActive !== "Round Trip"){
       setTimeout(() => {
@@ -185,11 +203,11 @@ console.log(this.isViewPartner)
         }
       }, 50);
     }
-
+   */
   }
 
   currentPeriodArrivalClicked(datePicker:any) {
-  let date = datePicker.target.value
+  /*let date = datePicker.target.value
   if(date && this.navItemActive == "Round Trip"){
     setTimeout(() => {
       if(this.isMobile == false) {
@@ -200,65 +218,135 @@ console.log(this.isViewPartner)
         this.openTravellerBlock();
       }
     }, 50);
-  }
+  }*/
 }
-  getCityList(SearchCityName:string) {
-    this.sub = this._flightService
-      .getCityList(SearchCityName)
-      .subscribe((res: any) => {
-        this.cityList = res.hits.hits;
 
-      });
-  }
+        
+     searchAutoComplete($event,field,device) {
+       let keycode = $event.which;
+        if ($event.keyCode != 40 && $event.keyCode != 38 ){
+        if ($event.timeStamp - this.lastKeypress > 0) {
+        this.queryText = $event.target.value;
+        if(this.queryText && this.queryText.length > 0){
+ 
+        let searchParam = {
+        searchDisplayForm: 'flights',
+        queryText: this.queryText
+        };
 
-  fromList(evt: any) {
-    console.log('fg');
-    // console.log(evt.keyCode);
-    this.toFlightList = false;
-    this.fromFlightList = true;
-    this.SearchCityName = evt.target.value.trim().toLowerCase();
-    this.getCityList(evt.target.value.trim().toLowerCase());
+        this.es.esSearch(searchParam).subscribe(res => {
+        //On Enter Key Pressed
+         if(keycode==13){
+          if(res.hits.total > 0){
+                if(field=='fromCity'){
+                this.searchFlightFromHeader='Result';
+                this.flightFromOptions = this.defaultFlightOptions;
+                this.onFromClick(res.hits.hits[0],device);
+                }else{
+                this.searchFlightToHeader='Result';
+                this.flightToOptions = this.defaultFlightOptions;
+                this.onToClick(res.hits.hits[0],device);
+                }
+               
+           }
+        }
+       
+        if(field=='fromCity'){
+         this.searchFlightFromHeader='Result';
+        this.flightFromOptions=res.hits.hits;
+        }else{
+         this.searchFlightToHeader='Result';
+        this.flightToOptions=res.hits.hits;
+        }
+     
+        });
 
-  }
 
-  toList(evt: any) {
-    this.fromFlightList = false;
-    this.toFlightList = true;
-    this.SearchCityName = evt.target.value.trim().toLowerCase();
-    this.getCityList(evt.target.value.trim().toLowerCase());
-  }
-
-  selectFromFlightList(para1: any) {
-        this.flightData.get('flightfrom').setValue(para1.id);
-        this.flightData.get('fromCity').setValue(para1.city);
-        this.flightData.get('fromContry').setValue(para1.country_code);
-        this.flightData.get('fromAirportName').setValue(para1.airport_name);
-
-        this.fromAirpotName = para1.airport_name;
-        this.fromCityName = para1.city;
-
-        this.fromFlightList = false;
-        let removeClassToCity = document.getElementById('removeClassToCity');
-        removeClassToCity?.classList.remove('flight-from-hide');
+       }else{
+       
+        if(field=='fromCity'){
+         this.flightFromOptions= this.defaultFlightOptions;
+         this.searchFlightFromHeader='Popular Cities';
+        }else{
+        this.flightToOptions= this.defaultFlightOptions;
+         this.searchFlightToHeader='Popular Cities';
+        }
+       
+       }
+      }
+     }
+    }
+   onFromClick(values,device) {
+        values=values['_source'];
+        this.flightData['controls']['fromCity'].setValue(values.city);
+        this.flightData['controls']['flightfrom'].setValue(values.airport_code);
+        this.flightData['controls']['fromContry'].setValue(values.country_code);
+        this.flightData['controls']['fromAirportName'].setValue(values.airport_name);
+        this.flightFromOptions= this.defaultFlightOptions;
+        this.fromAirpotName = values.airport_name;
+        this.fromCityName = values.city;
+       
         setTimeout(() => {
         let toCityDivElement:any=document.getElementById("toCityDiv");
         toCityDivElement?.click();
         this.toCityInput.nativeElement.focus();
-        }, 50);
+        }, 100);
 
   }
 
-  selectToFlightList(para2: any) {
-        this.flightData.get('flightto').setValue(para2.id);
-        this.flightData.get('toCity').setValue(para2.city);
-        this.flightData.get('toContry').setValue(para2.country_code);
-        this.flightData.get('toAirportName').setValue(para2.airport_name);
-        this.toAirpotName = para2.airport_name;
-        this.toCityName = para2.city;
-        this.toFlightList = false;
+  onToClick(values,device) {
+        values=values['_source'];   
+        this.flightData['controls']['toCity'].setValue(values.city);
+        this.flightData['controls']['flightto'].setValue(values.airport_code);
+        this.flightData['controls']['toContry'].setValue(values.country_code);
+        this.flightData['controls']['toAirportName'].setValue(values.airport_name);
+        this.toAirpotName = values.airport_name;
+        this.toCityName = values.city;
+         setTimeout(() => {
         this.datePicker.open();
-  }
+        $('.flight-to-data').addClass('flight-from-hide');
+      }, 100);
 
+  }
+         minDateFlightToMlite =  new Date();
+          minDateMlite =  new Date();
+   openMliteDatePicker(field,rtype){
+     
+     if(rtype==1){
+      this.navItemActive = "Round Trip"
+      }
+         
+        if(field=='departure'){
+        $('#flight_arrival_mlite').modal('hide');
+        $('#flight_departure_mlite').modal('show');
+        }else{
+        $('#flight_arrival_mlite').modal('show');
+        $('#flight_departure_mlite').modal('hide');
+        }
+        
+   }
+   
+     onSelectMliteDate(event,field){
+        
+      if(field=='departure'){
+        this.departureDate =event;
+        this.minDateFlightToMlite=event;
+        var compare1 = new Date(event).getTime();
+        var compare2 = new Date(this.arrivalDate).getTime();
+        if(compare1 > compare2){
+        this.arrivalDate = event;
+        this.flightData['controls']['arrival'].setValue(event);
+        }
+        
+        
+      }else{
+       this.arrivalDate = event;
+    }
+
+     
+  }
+   
+   
   adultsVal:any
   childVal:any
   infantsVal:any
@@ -418,9 +506,6 @@ console.log(this.isViewPartner)
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
 
-        this._styleManager.removeStyle('bootstrap-select');
-        this._styleManager.removeStyle('daterangepicker');
-        this._styleManager.removeScript('bootstrap-select');
         this._styleManager.removeScript('custom');
   }
 
