c826b6389c4c1381587011107cede8d5b511c43e
Tamil <tamil.selvan@reward360.co>
2022-08-10 15:04:58 +0530

    Code merged

---
+++ src/app/css-loader.component.ts
@@ -0,0 +1,53 @@
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Location } from '@angular/common';
+@Component({
+  selector: 'css-loader',
+  template: `<link rel="stylesheet" type="text/css" [href]="path">`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class CssLoaderComponent {
+  cssPath: any;
+
+  constructor(public sanitizer: DomSanitizer,private location:Location,
+    private cd: ChangeDetectorRef) {
+    
+    	let urlToSplit =this.location.path();
+	let unification =urlToSplit.split("/");
+
+	switch (unification[1]) {
+	case ('diners'):
+         this.cssPath = `smartbuy.css`;
+	break;
+	case ('infinia'):
+	 this.cssPath = `smartbuy.css`;
+	break;
+	case ('regalia'):
+	 this.cssPath = `smartbuy.css`;
+	break;
+         case ('regalia_gold'):
+	 this.cssPath = `regalia_gold.css`;
+	break;
+	case ('corporate'):
+	 this.cssPath = `smartbuy.css`;
+	break;
+	case ('business'):
+	 this.cssPath = `smartbuy.css`;
+	break;
+	default:
+	this.cssPath = `smartbuy.css`;
+	break;
+	}  
+      console.log(this.cssPath);
+      this.cd.markForCheck();
+  }
+
+  set path(path) {
+    this.cssPath = path;
+  }
+
+  get path() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.cssPath);
+  }
+}
