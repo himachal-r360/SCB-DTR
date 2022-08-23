f1a965e68f1804d9dbd7a83be540be82f3fa12ae
TamilSelvan <tamil.selvan@reward360.co>
2022-08-23 19:24:04 +0530

    Code merged

--- src/app/flight/flight-checkout/flight-checkout.component.ts
+++ src/app/flight/flight-checkout/flight-checkout.component.ts
@@ -2520,7 +2520,7 @@ export class FlightCheckoutComponent implements OnInit, OnDestroy {
         this.baggageInfo+= `<li role="presentation" >
         <a data-bs-target="#baggagetab" aria-controls="baggagetab" role="tab" data-bs-toggle="tab" aria-expanded="true" class="active" >
         <div class="rules-flight-items">
-        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCode+`.gif"     alt="6E" class=" mr-10"></div>
+        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCode+`.gif"     alt="`+airlineCodeR+`" class=" mr-10"></div>
         <div class="rules-flight-content">
         <h6>`+this.searchData['flightfrom']+` <img src="`+this.cdnUrl+`/images/icons/flight-right.png" alt="">`+this.searchData['flightto']+`</h6>
         </div>
@@ -2532,7 +2532,7 @@ export class FlightCheckoutComponent implements OnInit, OnDestroy {
         this.baggageInfo+= `<li role="presentation" >
         <a data-bs-target="#baggagetabR" aria-controls="baggagetabR" role="tab" data-bs-toggle="tab" aria-expanded="true"  >
         <div class="rules-flight-items">
-        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCodeR+`.gif"     alt="6E" class=" mr-10"></div>
+        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCodeR+`.gif"     alt="`+airlineCodeR+`" class=" mr-10"></div>
         <div class="rules-flight-content">
         <h6>`+this.searchData['flightto']+` <img src="`+this.cdnUrl+`/images/icons/flight-right.png" alt="">`+this.searchData['flightfrom']+`</h6>
         </div>
@@ -2632,14 +2632,16 @@ export class FlightCheckoutComponent implements OnInit, OnDestroy {
         if(this.onwardAirlineMulti_multi[i])
         airlineCode='Multi';
         else
-        airlineCode=this.onward_airline_array_multi[0];
+        airlineCode=this.onward_airline_array_multi[i][0];
+        
+        console.log(airlineCode);
 
         if(i >0) aclass='';
 
         this.cancellationPolicyOnward+= `<li role="presentation" >
         <a data-bs-target="#onwardtab`+i+`" aria-controls="onwardtab`+i+`" role="tab" data-bs-toggle="tab" aria-expanded="true" class="`+aclass+`" >
         <div class="rules-flight-items">
-        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCode+`.gif"     alt="6E" class=" mr-10"></div>
+        <div class="rules-flight-thumbe"><img src="`+this.cdnUrl+`/images/airlines/`+airlineCode+`.gif"     alt="`+airlineCode+`" class=" mr-10"></div>
         <div class="rules-flight-content">
         <h6>`+this.searchData[i]['flightfrom']+` <img src="`+this.cdnUrl+`/images/icons/flight-right.png" alt="">`+this.searchData[i]['flightto']+`</h6>
         </div>
