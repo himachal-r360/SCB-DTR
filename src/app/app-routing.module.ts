7f994e7628ae9cfbf3663a037c2818a905694272
TamilSelvan <tamil.selvan@reward360.co>
2022-09-04 16:16:11 +0530

    Code merged

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,7 @@ import { BannersComponent } from './shared-components/banners/banners.component'
 import { RegaliaGoldModule } from './regalia-gold/regalia-gold.module';
 import { SharedComponentsModule } from './shared-components/shared-components.module';
 import { OrdersRetryComponent } from './orders-retry/orders-retry.component';
-import { AppConfig } from './configs/app.config';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
-/*import { DinersModule } from './diners/diners.module';
-import { InfiniaModule } from './infinia/infinia.module';
-*/
 const routes: Routes = [
   {
     path:"" ,   component:HomeComponent 
@@ -58,35 +54,16 @@ const routes: Routes = [
     path: 'partners',
     loadChildren: () => import('./partners/partners.module').then(m => m.PartnersModule)
   },
-  
   {
 
     path: 'flights',
     loadChildren: () => import('./flight/flight.module').then(m => m.FlightModule)
   },
-  
-
   {
 
     path: 'foryou',
     loadChildren: () => import('./foryou-tab/foryou-tab.module').then(m => m.ForyouTabModule)
   },
-    /* {
-    path: 'diners/foryou',
-    loadChildren: () => import('./foryou-tab/foryou-tab.module').then(m => m.ForyouTabModule)
-  },
- {
-    path: 'regalia_gold/foryou',
-    loadChildren: () => import('./foryou-tab/foryou-tab.module').then(m => m.ForyouTabModule)
-  }, 
-  {
-    path: 'infinia',
-    loadChildren: () => import('./infinia/infinia.module').then(m => m.InfiniaModule)
-  },
-  {
-    path: 'diners',
-    loadChildren: () => import('./diners/diners.module').then(m => m.DinersModule)
-  },*/
   {
     path: 'regalia_gold',
     loadChildren: () => import('./regalia-gold/regalia-gold.module').then(m => m.RegaliaGoldModule),
@@ -98,18 +75,16 @@ const routes: Routes = [
 
   },
   
-  { path: AppConfig.routes.error404, component: Error404PageComponent },
+  //{ path: '404', component: Error404PageComponent },
   // otherwise redirect to 404
-  { path: '**', redirectTo: '/' + AppConfig.routes.error404 },
+//  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
   imports: [
     RegaliaGoldModule,
     SharedComponentsModule,
-    //InfiniaModule,
-    //DinersModule,
-        RouterModule.forRoot(routes, {
+    RouterModule.forRoot(routes, {
       scrollPositionRestoration: 'enabled',
       anchorScrolling: 'enabled',
       onSameUrlNavigation: 'reload'
