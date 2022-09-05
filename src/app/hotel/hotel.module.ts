318dc7f9f8300ee7931c7d244131ad404379b49d
vikash <vikas.mishra@reward360.co>
2022-09-05 10:47:26 +0530

    Api intigration in hotel search

---
+++ src/app/hotel/hotel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightSearchModule } from '../flight-search/flight-search.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlightSearchModule
+  ]
+})
+export class HotelModule { }
