d2761096d08f980dd243cac359196baab8b253af
vikash <vikas.mishra@reward360.co>
2022-09-07 12:21:34 +0530

    hotel search functionality

--- src/app/hotel/hotel.module.ts
+++ src/app/hotel/hotel.module.ts
@@ -3,18 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HotelSearchModule } from './hotel-search/hotel-search.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material.module';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {path:'hotel-list',component:HotelListComponent}
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HotelListComponent
+  ],
   imports: [
     CommonModule,
     HotelSearchModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
     
-  ]
+  ],
+  exports: [RouterModule],
 })
 export class HotelModule { }
