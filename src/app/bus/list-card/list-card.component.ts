c2f7ae10f78a48557578cd2b4a065c52290b7ab4
Hemanth <hemanth.kumar@reward360.co>
2022-09-11 18:19:03 +0530

    redbus bugs

--- src/app/bus/list-card/list-card.component.ts
+++ src/app/bus/list-card/list-card.component.ts
@@ -62,6 +62,7 @@ export class ListComponent implements OnInit,AfterViewInit {
  showrow: boolean = false;
  @Input('rowvalue') rowvalue: number;
   totalFareBus: boolean = false;
+  mlite_passengerError: boolean = false;
  @Input() set isrtc(p: boolean) {
   this.busId=this.bus.id;
   if (this.bus.rtc == false) {
@@ -575,24 +576,26 @@ this.cdnUrl = environment.cdnUrl+this.sg['assetPath'];
     this.maxSeatMessage = 'You can book up to ' + maxseats + ' passengers on a single ticket1';
     this.maxSeatMessageStatus = true;
     if (mobile == true) {
-     let body = document.getElementsByTagName('body')[0];
+      this.mlite_passengerError = true ;
+     /* let body = document.getElementsByTagName('body')[0];
      body.classList.remove("noscroll"); //remove the class
      var messageMaxSeat = 'You can book up to ' + maxseats + ' passengers on a single ticket2';
      const dialogRef = this.dialog.open(ConfirmationDialog, {
       id: 'messageforMliteDialog',
-      //panelClass: 'messageforMlitePanel',
-      // backdropClass: 'messageforMliteBackdrop',
+      panelClass: 'messageforMlitePanel',
+       backdropClass: 'messageforMliteBackdrop',
       data: {
        messageData: messageMaxSeat,
        redirectUrl: 0
       }
-     });
+     }); */
     }
     return true;
    } else {
     this.maxSeatMessageStatus = false;
    }
    this.maxSeatMessage = "";
+  
    this.selectedseats.push(seat); //REmove If Seat Selected Else Push inside array
   }
   var seatdetailss = this.busHelper.seatdetails(this.selectedseats);
@@ -641,7 +644,7 @@ this.show_earnpoints_text=this.commonHelper.get_service_earn_points(String(cardT
   } else {
    this.BoardingPoint = false;
   }
-
+  this.mlite_passengerError = false;
   /*let trackUrlParams = new HttpParams()
 	.set('current_url', window.location.href)
 	.set('category', 'RedBus')
