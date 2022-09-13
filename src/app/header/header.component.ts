540f10a04659ddbcb3a5956385a41674fcdee69f
TamilSelvan <tamil.selvan@reward360.co>
2022-09-13 19:24:11 +0530

    Code merged

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -795,7 +795,7 @@ closeCookieConsent(value){
        this.router.events.subscribe((event: any) => {
 	if (event instanceof NavigationEnd) {
 	
-	 if (event.url.includes("train-traveller") || event.url.includes("bus/booking") ||  event.url.includes("freshmenu/review-order") ) 
+	 if (event.url.includes("train/checkout") || event.url.includes("bus/checkout")  || event.url.includes("flight-checkout")  ) 
 	this.loginUrl='check-login';
 	else
 	this.loginUrl='check-login?g=1';
