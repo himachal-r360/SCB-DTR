a4397630a8313a0735ab1d017408c2c6f67e4346
TamilSelvan <tamil.selvan@reward360.co>
2022-09-21 13:05:06 +0530

    Code merged

--- src/app/hotel/hotel.module.ts
+++ src/app/hotel/hotel.module.ts
@@ -19,10 +19,15 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import { CouponsModule } from 'src/app/coupons/coupons.module';
 import { PaymentModule } from 'src/app/payment/payment.module';
 import { AmenitiesFilterPipe } from '../pipes/amenities-filter.pipe';
+import { HotelBookingRetryComponent } from './hotel-booking-retry/hotel-booking-retry.component';
 const routes: Routes = [
   {path:"hotel-list",component:HotelListComponent},
   {path:"hotel-detail",component:HotelDetailComponent},
-  {path:"hotel-checkout",component:HotelCheckoutComponent}
+  {path:"hotel-checkout",component:HotelCheckoutComponent},
+    {
+    path:"hotel-booking/retry" ,component:HotelBookingRetryComponent,
+
+  }
 ]
 
 @NgModule({
@@ -30,7 +35,7 @@ const routes: Routes = [
     HotelListComponent,
     HotelDetailComponent,
     HotelCheckoutComponent,
-    AmenitiesFilterPipe
+    AmenitiesFilterPipe,HotelBookingRetryComponent
   ],
   imports: [
     CommonModule,
