3f88cee499d16b5f2ade57681e547d69a8c73175
Afsal <abbasafsal.bm@reward360.co>
2022-10-06 19:00:38 +0530

    partner id added

--- src/app/paywithpoints/paywithpoints.component.ts
+++ src/app/paywithpoints/paywithpoints.component.ts
@@ -106,6 +106,7 @@ otperrormsg :any;
  resendOTPdiv:Boolean=true;
  domainRedirect:string;
  domainPath:string;
+ partnerId:number;
 dateInputMask = createMask<Date>({
     alias: 'datetime',
     inputFormat: 'dd/mm/yyyy',
@@ -168,6 +169,9 @@ dateInputMask = createMask<Date>({
         }
         
         ngOnChanges(changes: SimpleChanges): void {
+          let service_id = this.serviceId.toLowerCase();
+          let partnername = this.Partnertoken.toLowerCase();        
+          this.partnerId = this.serviceSettings['partners'][service_id][partnername];
         this.orderamount= Number(this.payTotalFare);
         this.setSlider();
         }
@@ -480,7 +484,7 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
                 "bin":this.cardaddForm1.controls['first4digit'].value,
                 "clientToken":this.sg['domainName'].toUpperCase(),
                 "services_id":this.serviceId,
-                "partner_id":42,
+                "partner_id":this.partnerId,
                 "modal":"DIGITAL",
                 "noopt": 1,
                 "savecard":1,
@@ -646,8 +650,9 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
         var last4digit = this.voucherForm1.controls['last4digit'].value;
         var applymobile = this.voucherForm1.controls['applymobile'].value;
         var dob = this.voucherForm1.controls['dob'].value;
-        var dobsplit =dob.split('/');
-        var dobStr = dobsplit[1]+'/'+dobsplit[0]+'/'+dobsplit[2];
+        // var dobsplit =dob.split('/');
+        // var dobStr = dobsplit[1]+'/'+dobsplit[0]+'/'+dobsplit[2];
+        var dobStr= dob;
         this.applyvouchercode = this.voucherForm1.controls['applyvouchercode'].value;
         if(this.XSRFTOKEN==undefined){
           this.XSRFTOKEN = this.sg['customerInfo']['XSRF-TOKEN'];
@@ -659,7 +664,7 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
         "mobile": applymobile,
         "DOB": dobStr,
         // "bin": first9digit,
-        "partner_id": 42,
+        "partner_id": this.partnerId,
         "services_id": this.serviceId,
         "total_amount": this.payTotalFare,
         "applyvouchercode": this.applyvouchercode,
@@ -732,7 +737,7 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
            //"bin":this.cardaddForm1.controls['first4digit'].value,
            "clientToken":this.sg['domainName'].toUpperCase(),
            "services_id":this.serviceId,
-           "partner_id":42,
+           "partner_id":this.partnerId,
            "modal":"DIGITAL",
            "noopt": 1,
            "savecard":1,
@@ -773,7 +778,7 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
         "bin":first9digit,
         "clientToken":this.sg['domainName'].toUpperCase(),
         "services_id":this.serviceId,
-        "partner_id":42,
+        "partner_id":this.partnerId,
         "modal":"DIGITAL",
         "noopt": 1,
         "savecard":1,
@@ -835,7 +840,7 @@ dateValidator(control: FormControl): { [s: string]: boolean } {
            "type":"available_points",
            "clientToken":this.sg['domainName'].toUpperCase(),
            "services_id":this.serviceId,
-           "partner_id":42,
+           "partner_id":this.partnerId,
            "modal":"DIGITAL",
            "noopt": 1,
            "savecard":savecard,
