7fd396cf77407bf83c2821c487ff2b3f714d691f
TamilSelvan <tamil.selvan@reward360.co>
2022-10-11 14:10:40 +0530

    Code merged

--- src/app/flight/flight-multicity/flight-multicity.component.ts
+++ src/app/flight/flight-multicity/flight-multicity.component.ts
@@ -93,16 +93,7 @@ export class FlightMulticityComponent implements OnInit, AfterViewInit ,OnDestro
     { name: 'All_Flights', active: true, value: 'All Flights' },
     { name: 'no_stops', active: false, value: 'Non-Stop' },
   ]
-  priceSortingFilteritems = [
-    { name: 'P_L_H', active: true, value: 'Low to High' ,image: './assets/images/icons/price-l.svg',activeImage:'./assets/images/icons/active_lth.svg', sortValue:'Price'},
-    { name: 'P_H_L', active: false, value: 'High to Low' , image:'./assets/images/icons/price-h.svg',activeImage:'./assets/images/icons/active_htl.svg',sortValue:'Price' },
-    { name: 'D_Short', active: false, value: 'Shortest' ,image:'./assets/images/icons/clock.svg',activeImage:'./assets/images/icons/active_duration.svg',sortValue:'Duration'},
-    { name: 'D_Long', active: false, value: 'Longest',image:'./assets/images/icons/clock.svg',activeImage:'./assets/images/icons/active_duration.svg',sortValue:'Duration'},
-    { name: 'D_E', active: false, value: 'Earliest' , image:'./assets/images/icons/Departure.svg',activeImage:'./assets/images/icons/active_departure.svg',sortValue:'Departure'},
-    { name: 'D_L', active: false, value: 'Latest' ,image:'./assets/images/icons/Departure.svg',activeImage:'./assets/images/icons/active_departure.svg',sortValue:'Departure'},
-    { name: 'A_E', active: false, value: 'Earliest',image:'./assets/images/icons/Arrival.svg',activeImage:'./assets/images/icons/active_arrival.svg', sortValue:'Arrival'},
-    { name: 'A_L', active: false, value: 'Latest',image:'./assets/images/icons/Arrival.svg',activeImage:'./assets/images/icons/active_arrival.svg', sortValue:'Arrival'},
-  ]
+  priceSortingFilteritems = [  ];
   RefundableFaresCount: number = 0;
   nonStopCount: number = 0;
   morningDearptureCount: number = 0;
@@ -160,6 +151,19 @@ export class FlightMulticityComponent implements OnInit, AfterViewInit ,OnDestro
 
   constructor(private route: ActivatedRoute, private _flightService: FlightService, private EncrDecr: EncrDecrService, private sg: SimpleGlobal, private scroll: ViewportScroller, public rest: RestapiService, private router: Router,private ngZone:NgZone) {
     this.cdnUrl = environment.cdnUrl + this.sg['assetPath'];
+    
+     this.priceSortingFilteritems = [
+    { name: 'P_L_H', active: true, value: 'Low to High' ,image: this.cdnUrl+'images/icons/price-l.svg',activeImage:this.cdnUrl+'images/icons/active_lth.svg', sortValue:'Price'},
+    { name: 'P_H_L', active: false, value: 'High to Low' , image:this.cdnUrl+'images/icons/price-h.svg',activeImage:this.cdnUrl+'images/icons/active_htl.svg',sortValue:'Price' },
+    { name: 'D_Short', active: false, value: 'Shortest' ,image:this.cdnUrl+'images/icons/clock.svg',activeImage:this.cdnUrl+'images/icons/active_duration.svg',sortValue:'Duration'},
+    { name: 'D_Long', active: false, value: 'Longest',image:this.cdnUrl+'images/icons/clock.svg',activeImage:this.cdnUrl+'images/icons/active_duration.svg',sortValue:'Duration'},
+    { name: 'D_E', active: false, value: 'Earliest' , image:this.cdnUrl+'images/icons/Departure.svg',activeImage:this.cdnUrl+'images/icons/active_departure.svg',sortValue:'Departure'},
+    { name: 'D_L', active: false, value: 'Latest' ,image:this.cdnUrl+'images/icons/Departure.svg',activeImage:this.cdnUrl+'images/icons/active_departure.svg',sortValue:'Departure'},
+    { name: 'A_E', active: false, value: 'Earliest',image:this.cdnUrl+'images/icons/Arrival.svg',activeImage:this.cdnUrl+'images/icons/active_arrival.svg', sortValue:'Arrival'},
+    { name: 'A_L', active: false, value: 'Latest',image:this.cdnUrl+'images/icons/Arrival.svg',activeImage:this.cdnUrl+'images/icons/active_arrival.svg', sortValue:'Arrival'},
+  ];
+    
+    
   /*  $(window).scroll(function (this) {
       if ($(window).scrollTop() + $(window).height() > $(document).height() - 300) {
         $('#endOfPage').trigger('click');
