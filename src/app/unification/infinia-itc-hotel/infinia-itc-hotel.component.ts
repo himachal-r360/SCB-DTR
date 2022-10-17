701afaedd94c0e6c8cfe5a687d655f4a52950160
vikash <vikas.mishra@reward360.co>
2022-10-17 17:20:24 +0530

    infinia-itc-hotel mlite view UI

--- src/app/unification/infinia-itc-hotel/infinia-itc-hotel.component.ts
+++ src/app/unification/infinia-itc-hotel/infinia-itc-hotel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-infinia-itc-hotel',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infinia-itc-hotel.component.scss']
 })
 export class InfiniaItcHotelComponent implements OnInit {
+  isMobile:boolean = true;
+  customOptions: OwlOptions = {
+    loop: false,
+    autoplay:false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    margin: 10,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
 
-  constructor() { }
+
+
+  constructor(private ngZone:NgZone) { 
+    window.onresize = (e) =>
+    {
+        this.ngZone.run(() => {
+          this.isMobile = window.innerWidth < 991 ? true : false;
+        });
+    };
+  }
 
   ngOnInit(): void {
+    this.isMobile = window.innerWidth < 991 ? true : false;
   }
 
 }
